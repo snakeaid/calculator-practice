9c9253f8bd942e64494a8a7fe2a9c92782b43f71
illia <illia.antypenko@gmail.com>
2024-11-19 00:38:07 +0200

    Implement pratt parser

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use std::io::{self, BufRead};
 use pest::Parser;
+use pest::iterators::Pairs;
 use pest_derive::Parser;
 
 #[derive(Parser)]
@@ -23,6 +25,53 @@ pub enum Op {
     Divide,
 }
 
-fn main() {
-    println!("Calculator with AST initialized!");
+lazy_static::lazy_static! {
+    static ref PRATT_PARSER: pest::pratt_parser::PrattParser<Rule> = {
+        use pest::pratt_parser::{Assoc::*, Op};
+        use Rule::*;
+
+        pest::pratt_parser::PrattParser::new()
+            .op(Op::infix(add, Left) | Op::infix(subtract, Left))
+            .op(Op::infix(multiply, Left) | Op::infix(divide, Left))
+    };
+}
+
+pub fn parse_expr(pairs: Pairs<Rule>) -> Expr {
+    PRATT_PARSER
+        .map_primary(|primary| match primary.as_rule() {
+            Rule::integer => Expr::Integer(primary.as_str().parse::<i32>().unwrap()),
+            rule => unreachable!("Expr::parse expected atom, found {:?}", rule)
+        })
+        .map_infix(|lhs, op, rhs| {
+            let op = match op.as_rule() {
+                Rule::add => Op::Add,
+                Rule::subtract => Op::Subtract,
+                Rule::multiply => Op::Multiply,
+                Rule::divide => Op::Divide,
+                rule => unreachable!("Expr::parse expected infix operation, found {:?}", rule),
+            };
+            Expr::BinOp {
+                lhs: Box::new(lhs),
+                op,
+                rhs: Box::new(rhs),
+            }
+        })
+        .parse(pairs)
+}
+
+fn main() -> io::Result<()> {
+    for line in io::stdin().lock().lines() {
+        match CalculatorParser::parse(Rule::equation, &line?) {
+            Ok(mut pairs) => {
+                println!(
+                    "Parsed: {:#?}",
+                    parse_expr(pairs.next().unwrap().into_inner())
+                );
+            }
+            Err(e) => {
+                eprintln!("Parse failed: {:?}", e);
+            }
+        }
+    }
+    Ok(())
 }
