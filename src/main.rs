96f4d9afb44505f347a826fc7168d246c79cc3c6
illia <illia.antypenko@gmail.com>
2024-11-19 00:34:29 +0200

    Add AST

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+pub struct CalculatorParser;
+
+#[derive(Debug)]
+pub enum Expr {
+    Integer(i32),
+    BinOp {
+        lhs: Box<Expr>,
+        op: Op,
+        rhs: Box<Expr>,
+    },
+}
+
+#[derive(Debug)]
+pub enum Op {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Calculator with AST initialized!");
 }
